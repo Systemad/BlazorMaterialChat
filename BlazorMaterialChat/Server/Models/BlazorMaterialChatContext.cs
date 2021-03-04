e3270300c87d0501b49f668370136095bdb135d0
systemad <dannesuke@gmail.com>
2021-03-04 15:44:31 +0100

    Add missing files

---
+++ BlazorMaterialChat/Server/Models/BlazorMaterialChatContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace BlazorMaterialChat.Server.Models
+{
+    public partial class BlazorMaterialChatContext : DbContext
+    {
+        public BlazorMaterialChatContext()
+        {
+        }
+
+        public BlazorMaterialChatContext(DbContextOptions<BlazorMaterialChatContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Name=BlazorMaterialChat");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Password).IsRequired();
+
+                entity.Property(e => e.Username).IsRequired();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
