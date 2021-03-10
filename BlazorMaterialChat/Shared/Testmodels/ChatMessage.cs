5fe00d755c4f59df3c23bcb8ce888b01b790cd70
systemad <dannesuke@gmail.com>
2021-03-10 14:47:14 +0100

    Navigation menu: fixed and remove unwanted entries

---
+++ BlazorMaterialChat/Shared/Testmodels/ChatMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorMaterialChat.Shared.Testmodels
+{
+    public class ChatMessage
+    {
+        public string UserName { get; set; }
+        public string Message { get; set; }
+    }
+}
