a2264e43e9a8e4136f1aab1b841e0f9d280582f3
systemad <dannesuke@gmail.com>
2021-03-13 22:10:09 +0100

    Reworked chathub

--- BlazorMaterialChat/Server/Hubs/ChatHub.cs
+++ BlazorMaterialChat/Server/Hubs/ChatHub.cs
@@ -1,16 +1,54 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlazorMaterialChat.Shared.Helpers;
 using BlazorMaterialChat.Shared.Models;
 using Microsoft.AspNetCore.SignalR;
+using MudBlazor;
 
 namespace BlazorMaterialChat.Server.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+
+        private static readonly Dictionary<string, string> UserLookup = new Dictionary<string, string>();
+        
+        
+        public async Task SendMessage(string username, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync(Messages.RECEIVE, username, message);
+        }
+
+        public async Task Register(string username)
+        {
+            var curentId = Context.ConnectionId;
+            if (!UserLookup.ContainsKey(curentId))
+            {
+                UserLookup.Add(curentId, username);
+                await Clients.AllExcept(curentId).SendAsync(
+                    Messages.RECEIVE, 
+                    username, $"{username} has joined the chat");
+            }
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            string id = Context.ConnectionId;
+            if (!UserLookup.TryGetValue(id, out string username))
+                username = "[username]";
+
+            UserLookup.Remove(id);
+            
+            await Clients.AllExcept(id).SendAsync(
+                    Messages.RECEIVE, 
+                    username, $"{username} has left the chat");
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
---
+++ BlazorMaterialChat/Shared/Helpers/Messages.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorMaterialChat.Shared.Helpers
+{
+    public static class Messages
+    {
+        public const string RECEIVE = "ReceiveMessage";
+        
+        public const string REGISTER = "Register";
+        
+        public const string SEND = "SendMessage";
+
+    }
+}
